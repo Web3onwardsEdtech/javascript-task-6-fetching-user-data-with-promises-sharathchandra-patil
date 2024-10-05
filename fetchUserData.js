e486c7089614b9233d80c3ecbae0591763d7554c
Sharathchandra <sharathchandra9204@gmail.com>
2024-10-05 16:53:56 +0530

    Task 6 submission

--- fetchUserData.js
+++ fetchUserData.js
@@ -1,1 +1,20 @@
-
+function fetchUserData() {
+    return new Promise((resolve, reject) => {
+        fetch('https://jsonplaceholder.typicode.com/users/100')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                resolve({ name: data.name, email: data.email });
+            })
+            .catch(error => {
+                reject('Error fetching user data');
+            });
+    });
+}
+fetchUserData()
+    .then(data => console.log(data))       
+    .catch(error => console.log(error));  
